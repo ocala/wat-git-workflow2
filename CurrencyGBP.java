58bccbdc945c652f4768edbb9dbebc70454265f9
Camilo Esparza <camilo.esparza@boszdigital.com>
2013-07-09 16:32:23 -0500

    Arreglo que Mapi deb'ia hacer

---
+++ CurrencyGBP.java
@@ -0,0 +1,19 @@
+public  class  CurrencyGBP extends Currency{
+	
+
+
+public Currency(){
+   super("england",1, 1250);
+}
+
+public double getValue(){	
+return this.value;
+}
+
+public void setValue(double value){
+	
+	this.value=value;
+}
+
+
+}
