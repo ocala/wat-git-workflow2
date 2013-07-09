317c48c6ff30f6aa0e16509049560bc68c0152a5
Maria Paula Forero <mariapaula.forero@boszdigital.com>
2013-07-09 15:42:16 -0500

    Add CurrencyConverter implementation

---
+++ USDConverter.java
@@ -0,0 +1,8 @@
+
+public class USDConverter implements CurrencyConverter{
+
+	public Currency convert(Currency source, Currency destination, double value){
+		Currency c = new Currency();
+		retrun c;
+	}
+}
