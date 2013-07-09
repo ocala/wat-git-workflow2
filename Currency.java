8bc70119a084bfadf3a1be1791b25075019d8102
Camilo Esparza <camilo.esparza@boszdigital.com>
2013-07-09 15:37:31 -0500

    Abstract class currency

---
+++ Currency.java
@@ -0,0 +1,18 @@
+public abstract class Currency{
+	
+public String country;
+public double valueUSD;
+public double value;
+
+public double getValue(){
+	
+return this.value;
+}
+
+public void setValue(double value){
+	
+	this.value=value;
+}
+
+
+}
